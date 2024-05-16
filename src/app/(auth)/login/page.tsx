08ae5d84d4eb0a47724075a28122ddb3482f1f12
Rizki Pandiwa <rizki.pandiwa@erajaya.com>
2024-05-17 00:07:00 +0700

    test deploy

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 // react/no-unescaped-entities
+
 import { login } from "@/services/authTrService";
 import { StoreCookie } from "@/utils/configCookie";
 import { useForm } from "react-hook-form";
@@ -32,110 +33,112 @@ export default function Login() {
     }
     setIsLoading(false);
   };
-  return (
-    <div className="bg-gray-100 flex h-screen items-center justify-center p-4">
-      <div className="w-full max-w-md">
-        <div className="bg-white shadow-md rounded-md p-8">
-          <h1 className="mt-6 text-center text-md font-bold tracking-tight text-gray-900">
-            TR-APP
-          </h1>
+  if (typeof window !== "undefined") {
+    return (
+      <div className="bg-gray-100 flex h-screen items-center justify-center p-4">
+        <div className="w-full max-w-md">
+          <div className="bg-white shadow-md rounded-md p-8">
+            <h1 className="mt-6 text-center text-md font-bold tracking-tight text-gray-900">
+              TR-APP
+            </h1>
 
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-          {errorMessage && (
-            <div
-              className="flex  justify-between bg-red-100 border border-red-400 text-red-700 px-4 py-3 my-3 rounded  "
-              role="alert"
-            >
-              <span className="block sm:inline ">{errorMessage}</span>
-              <span className="inline" onClick={() => setErrorMessage("")}>
-                <svg
-                  className="fill-current h-6 w-6"
-                  role="button"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <title>Close</title>
-                  <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-                </svg>
-              </span>
-            </div>
-          )}
-
-          <div className="space-y-6 mt-4">
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Email
-              </label>
-              <div className="mt-1">
-                <input
-                  className="text-black px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm  sm:text-sm"
-                  {...register("email", {
-                    required: {
-                      value: true,
-                      message: "This field cannot be empty",
-                    },
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                      message: "Invalid email address",
-                    },
-                  })}
-                />
-
-                <span className="text-red-500 text-sm">
-                  {errors.email?.message}
-                </span>
-              </div>
-            </div>
-
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Password
-              </label>
-              <div className="mt-1">
-                <input
-                  type="password"
-                  className="text-black px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm  sm:text-sm"
-                  {...register("password", {
-                    required: {
-                      value: true,
-                      message: "This field cannot be empty",
-                    },
-                  })}
-                />
-
-                <span className="text-red-500 text-sm">
-                  {errors.password?.message}
-                </span>
-              </div>
-            </div>
-
-            <div>
-              <button
-                onClick={handleSubmit(onSubmit)}
-                className="flex w-full justify-center rounded-md border border-transparent bg-[#0060AC] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+            {errorMessage && (
+              <div
+                className="flex  justify-between bg-red-100 border border-red-400 text-red-700 px-4 py-3 my-3 rounded  "
+                role="alert"
               >
-                {isLoading && <Loading />}
-                Login
-              </button>
-            </div>
-            <div className="flex justify-center items-center mt-4">
-              <p className="inline-flex items-center text-gray-700 font-medium text-xs text-center">
-                <span className="ml-2">
-                  You dont have an account?
-                  <Link
-                    href="/register"
-                    className="text-xs ml-2 text-blue-500 font-semibold"
+                <span className="block sm:inline ">{errorMessage}</span>
+                <span className="inline" onClick={() => setErrorMessage("")}>
+                  <svg
+                    className="fill-current h-6 w-6"
+                    role="button"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
                   >
-                    Register now
-                  </Link>
+                    <title>Close</title>
+                    <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                  </svg>
                 </span>
-              </p>
+              </div>
+            )}
+
+            <div className="space-y-6 mt-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Email
+                </label>
+                <div className="mt-1">
+                  <input
+                    className="text-black px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm  sm:text-sm"
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "This field cannot be empty",
+                      },
+                      pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: "Invalid email address",
+                      },
+                    })}
+                  />
+
+                  <span className="text-red-500 text-sm">
+                    {errors.email?.message}
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="password"
+                    className="text-black px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm  sm:text-sm"
+                    {...register("password", {
+                      required: {
+                        value: true,
+                        message: "This field cannot be empty",
+                      },
+                    })}
+                  />
+
+                  <span className="text-red-500 text-sm">
+                    {errors.password?.message}
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  onClick={handleSubmit(onSubmit)}
+                  className="flex w-full justify-center rounded-md border border-transparent bg-[#0060AC] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
+                >
+                  {isLoading && <Loading />}
+                  Login
+                </button>
+              </div>
+              <div className="flex justify-center items-center mt-4">
+                <p className="inline-flex items-center text-gray-700 font-medium text-xs text-center">
+                  <span className="ml-2">
+                    You dont have an account?
+                    <Link
+                      href="/register"
+                      className="text-xs ml-2 text-blue-500 font-semibold"
+                    >
+                      Register now
+                    </Link>
+                  </span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/app/(dashboard)/form/[id]/page.tsx
+++ src/app/(dashboard)/form/[id]/page.tsx
@@ -20,7 +20,6 @@ export default function Form({ params }: any) {
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isCreate, setIsCreate] = useState<boolean>(true);
-  const [captchaCode, setCaptchaCode] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string>("");
   const {
     formState: { errors },
@@ -68,193 +67,195 @@ export default function Form({ params }: any) {
     }
   }, [id]);
 
-  return (
-    <div className="p-10 text-black">
-      <h1 className="mb-8 font-extrabold text-4xl">
-        {isCreate ? "Add" : "Update"} Customer
-      </h1>
-      {errorMessage && (
-        <div
-          className="flex  justify-between bg-red-100 border border-red-400 text-red-700 px-4 py-3 my-3 rounded mb-3  "
-          role="alert"
-        >
-          <span className="block sm:inline ">{errorMessage}</span>
-          <span className="inline" onClick={() => setErrorMessage("")}>
-            <svg
-              className="fill-current h-6 w-6"
-              role="button"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <title>Close</title>
-              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-            </svg>
-          </span>
-        </div>
-      )}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <form>
-          <div>
-            <label className="block">No KTP</label>
-            <input
-              {...register("identity_card", {
-                required: {
-                  value: true,
-                  message: "This field cannot be empty",
-                },
-                maxLength: {
-                  value: 16,
-                  message: "Maximum length is 16 digits",
-                },
-                pattern: {
-                  value: /^[0-9]*$/,
-                  message: "Only numbers are allowed",
-                },
-              })}
-              className="w-full rounded bg-white p-2 border border-gray-300 block mt-1 "
-            />
-            <span className="text-red-500 text-sm">
-              {errors.identity_card?.message}
-            </span>
-          </div>
-
-          <div className="mt-4">
-            <label className="block ">Nama Lengkap</label>
-            <input
-              {...register("full_name", {
-                required: {
-                  value: true,
-                  message: "This field cannot be empty",
-                },
-              })}
-              className="w-full rounded bg-white p-2 border border-gray-300 block mt-1 "
-            />
-
-            <span className="text-red-500 text-sm">
-              {errors.full_name?.message}
-            </span>
-          </div>
-
-          <div className="mt-4">
-            <label className="block font-semibold">Jenis Kelamin</label>
-            <div className="mt-1">
-              <label className="mr-4">
-                <input
-                  {...register("gender", {
-                    required: {
-                      value: true,
-                      message: "This field cannot be empty",
-                    },
-                  })}
-                  type="radio"
-                  name="gender"
-                  value="pria"
-                  className="mr-1"
-                />
-                Pria
-              </label>
-              <label>
-                <input
-                  {...register("gender", {
-                    required: {
-                      value: true,
-                      message: "This field cannot be empty",
-                    },
-                  })}
-                  type="radio"
-                  name="gender"
-                  value="wanita"
-                  className="mr-1"
-                />
-                Wanita
-              </label>
-            </div>
-            <span className="text-red-500 text-sm">
-              {errors.gender?.message}
-            </span>
-          </div>
-          <div className="mt-4">
-            <label className="block ">No HP</label>
-            <input
-              {...register("phone_number", {
-                required: {
-                  value: true,
-                  message: "This field cannot be empty",
-                },
-                pattern: {
-                  value: /^[0-9]*$/,
-                  message: "Only numbers are allowed",
-                },
-              })}
-              className="w-full rounded bg-white p-2 border border-gray-300 block mt-1 "
-            />
-            <span className="text-red-500 text-sm">
-              {errors.phone_number?.message}
-            </span>
-          </div>
-          <div className="mt-4">
-            <label className="block">Email</label>
-            <input
-              {...register("email", {
-                required: {
-                  value: true,
-                  message: "This field cannot be empty",
-                },
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: "Invalid email address",
-                },
-              })}
-              className="w-full rounded bg-white p-2 border border-gray-300 block mt-1 "
-            />
-            <span className="text-red-500 text-sm">
-              {errors.email?.message}
-            </span>
-          </div>
-
-          <div className="flex items-center justify-between mt-8">
-            <button
-              onClick={handleSubmit(Submit)}
-              className="flex items-center justify-center px-8  border border-black text-base font-medium rounded-md text-black bg-white  md:py-2  md:px-10 "
-            >
-              {isLoading && <Loading />} {isCreate ? "Submit" : "Update"}
-            </button>
-            <Link
-              href="/"
-              className="inline-flex items-center text-black  hover:text-gray-00"
-            >
+  if (typeof window !== "undefined") {
+    return (
+      <div className="p-10 text-black">
+        <h1 className="mb-8 font-extrabold text-4xl">
+          {isCreate ? "Add" : "Update"} Customer
+        </h1>
+        {errorMessage && (
+          <div
+            className="flex  justify-between bg-red-100 border border-red-400 text-red-700 px-4 py-3 my-3 rounded mb-3  "
+            role="alert"
+          >
+            <span className="block sm:inline ">{errorMessage}</span>
+            <span className="inline" onClick={() => setErrorMessage("")}>
               <svg
+                className="fill-current h-6 w-6"
+                role="button"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="h-6 w-6"
+                viewBox="0 0 20 20"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  strokeWidth="2"
-                  d="M7 16l-4-4m0 0l4-4m-4 4h18"
-                ></path>
+                <title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
               </svg>
-              <span className="ml-1 font-bold text-lg">Back</span>
-            </Link>
+            </span>
           </div>
-        </form>
+        )}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <form>
+            <div>
+              <label className="block">No KTP</label>
+              <input
+                {...register("identity_card", {
+                  required: {
+                    value: true,
+                    message: "This field cannot be empty",
+                  },
+                  maxLength: {
+                    value: 16,
+                    message: "Maximum length is 16 digits",
+                  },
+                  pattern: {
+                    value: /^[0-9]*$/,
+                    message: "Only numbers are allowed",
+                  },
+                })}
+                className="w-full rounded bg-white p-2 border border-gray-300 block mt-1 "
+              />
+              <span className="text-red-500 text-sm">
+                {errors.identity_card?.message}
+              </span>
+            </div>
 
-        <aside className="">
-          <div className="bg-gray-100 p-8 rounded">
-            <h2 className="font-bold text-2xl">Instructions</h2>
-            <ul className="list-disc mt-4 list-inside">
-              <li>Validasi panjang KTP harus 16 digit</li>
-              <li>Validasi email harus sesuai format email address</li>
-              <li>
-                User nontification jika ada input yang belum sesuai validasi
-              </li>
-            </ul>
-          </div>
-        </aside>
+            <div className="mt-4">
+              <label className="block ">Nama Lengkap</label>
+              <input
+                {...register("full_name", {
+                  required: {
+                    value: true,
+                    message: "This field cannot be empty",
+                  },
+                })}
+                className="w-full rounded bg-white p-2 border border-gray-300 block mt-1 "
+              />
+
+              <span className="text-red-500 text-sm">
+                {errors.full_name?.message}
+              </span>
+            </div>
+
+            <div className="mt-4">
+              <label className="block font-semibold">Jenis Kelamin</label>
+              <div className="mt-1">
+                <label className="mr-4">
+                  <input
+                    {...register("gender", {
+                      required: {
+                        value: true,
+                        message: "This field cannot be empty",
+                      },
+                    })}
+                    type="radio"
+                    name="gender"
+                    value="pria"
+                    className="mr-1"
+                  />
+                  Pria
+                </label>
+                <label>
+                  <input
+                    {...register("gender", {
+                      required: {
+                        value: true,
+                        message: "This field cannot be empty",
+                      },
+                    })}
+                    type="radio"
+                    name="gender"
+                    value="wanita"
+                    className="mr-1"
+                  />
+                  Wanita
+                </label>
+              </div>
+              <span className="text-red-500 text-sm">
+                {errors.gender?.message}
+              </span>
+            </div>
+            <div className="mt-4">
+              <label className="block ">No HP</label>
+              <input
+                {...register("phone_number", {
+                  required: {
+                    value: true,
+                    message: "This field cannot be empty",
+                  },
+                  pattern: {
+                    value: /^[0-9]*$/,
+                    message: "Only numbers are allowed",
+                  },
+                })}
+                className="w-full rounded bg-white p-2 border border-gray-300 block mt-1 "
+              />
+              <span className="text-red-500 text-sm">
+                {errors.phone_number?.message}
+              </span>
+            </div>
+            <div className="mt-4">
+              <label className="block">Email</label>
+              <input
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "This field cannot be empty",
+                  },
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
+                className="w-full rounded bg-white p-2 border border-gray-300 block mt-1 "
+              />
+              <span className="text-red-500 text-sm">
+                {errors.email?.message}
+              </span>
+            </div>
+
+            <div className="flex items-center justify-between mt-8">
+              <button
+                onClick={handleSubmit(Submit)}
+                className="flex items-center justify-center px-8  border border-black text-base font-medium rounded-md text-black bg-white  md:py-2  md:px-10 "
+              >
+                {isLoading && <Loading />} {isCreate ? "Submit" : "Update"}
+              </button>
+              <Link
+                href="/"
+                className="inline-flex items-center text-black  hover:text-gray-00"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    d="M7 16l-4-4m0 0l4-4m-4 4h18"
+                  ></path>
+                </svg>
+                <span className="ml-1 font-bold text-lg">Back</span>
+              </Link>
+            </div>
+          </form>
+
+          <aside className="">
+            <div className="bg-gray-100 p-8 rounded">
+              <h2 className="font-bold text-2xl">Instructions</h2>
+              <ul className="list-disc mt-4 list-inside">
+                <li>Validasi panjang KTP harus 16 digit</li>
+                <li>Validasi email harus sesuai format email address</li>
+                <li>
+                  User nontification jika ada input yang belum sesuai validasi
+                </li>
+              </ul>
+            </div>
+          </aside>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
