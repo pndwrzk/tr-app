8d58a34e50133e36fcc252f13dd204cc470eacb0
Rizki Pandiwa <rizki.pandiwa@erajaya.com>
2024-05-17 01:19:16 +0700

    fix render client side

--- src/utils/configCookie.ts
+++ src/utils/configCookie.ts
@@ -39,12 +39,14 @@ function setCookie(name: string, value: string) {
 }
 
 function getCookie(name: string) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  if (typeof document !== "undefined") {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
   }
   return null;
 }
