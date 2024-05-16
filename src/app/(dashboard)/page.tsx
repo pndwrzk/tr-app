0e63bb20629fddc45557d7d9da6daac57caa0d72
Rizki Pandiwa <rizki.pandiwa@erajaya.com>
2024-05-17 01:34:56 +0700

    set message error bulk delete

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -8,10 +8,13 @@ import {
 } from "@/services/customerTrService";
 import { useEffect, useState } from "react";
 import { customerData } from "@/types/customerTrTypes";
+import Loading from "@/components/loading";
 
 export default function Dashboard() {
   const [customers, setCustomers] = useState<customerData[]>([]);
   const [idBulk, setIdBulk] = useState<number[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [loadingBulk, setLoadingBulk] = useState<boolean>(false);
 
   const fetchAllCustomer = async () => {
     const respone = await getAllCustomer();
@@ -46,22 +49,24 @@ export default function Dashboard() {
     const resultStatus = result.status;
     const resultData = result.data;
     if (resultStatus !== 200) {
-      console.log(resultData.message);
+      setErrorMessage(resultData.message);
     } else {
       fetchAllCustomer();
     }
   };
 
   const handleBulkDelete = async () => {
+    setLoadingBulk(true);
     const result = await deleteCustomerBulk(idBulk);
     const resultStatus = result.status;
     const resultData = result.data;
     if (resultStatus !== 200) {
-      console.log(resultData.message);
+      setErrorMessage(resultData.message);
     } else {
       fetchAllCustomer();
       setIdBulk([]);
     }
+    setLoadingBulk(false);
   };
 
   useEffect(() => {
@@ -80,22 +85,27 @@ export default function Dashboard() {
                     onClick={handleBulkDelete}
                     className="text-black text-sm bg-white  border border-gray-500  rounded font-medium px-4 py-2 inline-flex space-x-1 items-center"
                   >
-                    <span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                        />
-                      </svg>
-                    </span>
+                    {loadingBulk ? (
+                      <Loading />
+                    ) : (
+                      <span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                          />
+                        </svg>
+                      </span>
+                    )}
+
                     <span className="hidden md:inline-block">Delete</span>
                   </button>
                 )}
@@ -107,10 +117,29 @@ export default function Dashboard() {
               </div>
             </div>
 
+            {errorMessage && (
+              <div
+                className="flex  justify-between bg-red-100 border border-red-400 text-red-700 px-4 py-3 my-3 rounded mb-3  "
+                role="alert"
+              >
+                <span className="block sm:inline ">{errorMessage}</span>
+                <span className="inline" onClick={() => setErrorMessage("")}>
+                  <svg
+                    className="fill-current h-6 w-6"
+                    role="button"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <title>Close</title>
+                    <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                  </svg>
+                </span>
+              </div>
+            )}
+
             <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5 border  mt-4">
               <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                 <div className="overflow-hidden">
-                  <div></div>
                   <table className="min-w-full">
                     <thead className="bg-white border-b">
                       <tr>
